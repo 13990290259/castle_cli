837f2aa5fe93e203d8a7754336afc59f821ab9a3
13990290259 <39643154+13990290259@users.noreply.github.com>
2018-07-26 19:06:24 +0800

    Update modules.ts

--- template/vuex/modules.ts
+++ template/vuex/modules.ts
@@ -54,10 +54,10 @@ const state: State = {
 //定义getters
 const getters = {
     [G_{__UPPER_MODULE_NAME__}_ALL]() {
-        if (state.All.length == 0 && isDic) {
-    store.dispatch(A_{__UPPER_MODULE_NAME__}_ALL)
-}
-return state.All;
+    if (state.All.length == 0 && isDic) {
+        store.dispatch(A_{__UPPER_MODULE_NAME__}_ALL)
+    }
+        return state.All;
     },
 [G_{__UPPER_MODULE_NAME__}]() {
     return state.Result;
@@ -68,28 +68,28 @@ const actions = {
     // 处理搜索请求
     async [A_{__UPPER_MODULE_NAME__}_SEARCH](context: ActionContextBasic) {
         let d = await {__MODULE_NAME__}.search(state.Where)
-context.commit(M_{__UPPER_MODULE_NAME__} _SEARCH, d);
+        context.commit(M_{__UPPER_MODULE_NAME__}_SEARCH, d);
     },
 //取出所有的值，仅仅针对字典型，其它类型禁止使用
-async[A_{__UPPER_MODULE_NAME__} _ALL](context: ActionContextBasic) {
+async [A_{__UPPER_MODULE_NAME__}_ALL](context: ActionContextBasic) {
     let d = await {__MODULE_NAME__}.search({ P: 1, N: 99999 })
     context.commit(M_{__UPPER_MODULE_NAME__}_ALL, d.L);
 },
-    async[A_{__UPPER_MODULE_NAME__} _ADD](context: ActionContextBasic, { Data, s, e }: { Data: {__MODULE_NAME__}Object, s?: Function, e?: Function }) {
+    async [A_{__UPPER_MODULE_NAME__}_ADD](context: ActionContextBasic, { Data, s, e }: { Data: {__MODULE_NAME__}Object, s?: Function, e?: Function }) {
         let d = await {__MODULE_NAME__}.add(Data)
         if (d) { if (isFunction(s)) s() } else if (isFunction(e)) { e() }
         //更新列表
         context.dispatch(A_{__UPPER_MODULE_NAME__}_SEARCH)
         context.dispatch(A_{__UPPER_MODULE_NAME__}_ALL)
     },
-        async[A_{__UPPER_MODULE_NAME__} _SAVE](context: ActionContextBasic, { Data, s, e }: { Data: {__MODULE_NAME__}Object, s?: Function, e?: Function }) {
+        async [A_{__UPPER_MODULE_NAME__}_SAVE](context: ActionContextBasic, { Data, s, e }: { Data: {__MODULE_NAME__}Object, s?: Function, e?: Function }) {
             let d = await {__MODULE_NAME__}.save(Data.{__MODULE_NAME__} ID, Data)
             if (d) { if (isFunction(s)) s() } else if (isFunction(e)) { e() }
             //更新列表
             context.dispatch(A_{__UPPER_MODULE_NAME__}_SEARCH)
             context.dispatch(A_{__UPPER_MODULE_NAME__}_ALL)
         },
-            async[A_{__UPPER_MODULE_NAME__} _DEL](context: ActionContextBasic, { {__MODULE_NAME__}ID, s, e }: { {__MODULE_NAME__}ID: number, s?: Function, e?: Function }) {
+            async [A_{__UPPER_MODULE_NAME__}_DEL](context: ActionContextBasic, { {__MODULE_NAME__}ID, s, e }: { {__MODULE_NAME__}ID: number, s?: Function, e?: Function }) {
                 let d = await {__MODULE_NAME__}.del({__MODULE_NAME__}ID)
                 if (d) { if (isFunction(s)) s() } else if (isFunction(e)) { e() }
                 //更新列表
