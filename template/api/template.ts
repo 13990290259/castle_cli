4c441e14ffd093dff4fd39d55be630cd96291628
曾腾辉 <857946849@qq.com>
2018-07-26 19:28:23 +0800

    api模板修改

--- template/api/template.ts
+++ template/api/template.ts
@@ -1,5 +1,6 @@
-import Request from './koa'
+import Request from 'caslte-request'
 class __MODULE_NAME__ extends Request{
+    Controller="__MODULE_NAME__"
     _pk="__MODULE_PK__"
 }
 const r = new __MODULE_NAME__();
